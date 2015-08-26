6c650baf7dbda0b0098c17536be6b9d9fc323e5e
Grantland Chew <grantlandchew@gmail.com>
2015-08-26 11:48:56 -0700

    Clean up unused code
    Unused

--- Parse/src/main/java/com/parse/ParseNotificationManager.java
+++ Parse/src/main/java/com/parse/ParseNotificationManager.java
@@ -43,10 +43,6 @@ import android.util.SparseIntArray;
   
   // protected by object lock
   private SparseIntArray iconIds = new SparseIntArray();
-
-  public boolean getShouldShowNotifications() {
-    return shouldShowNotifications;
-  }
   
   public void setShouldShowNotifications(boolean show) {
     shouldShowNotifications = show;
@@ -87,47 +83,6 @@ import android.util.SparseIntArray;
     return valid == 1;
   }
   
-  public Notification createNotification(Context context, String title, String body,
-      Class<? extends Activity> clazz, int iconId, Bundle extras) {
-    Notification notification = null;
-    
-    if (!isValidIconId(context, iconId)) {
-      PLog.e(TAG, "Icon id " + iconId + " is not a valid drawable. Trying to fall back to " +
-          "default app icon.");
-      iconId = ManifestInfo.getIconId();
-    }
-    
-    if (iconId == 0) {
-      PLog.e(TAG, "Could not find a valid icon id for this app. This is required to create a " +
-          "Notification object to show in the status bar. Make sure that the <application> in " +
-          "in your Manifest.xml has a valid android:icon attribute.");
-    } else if (context == null || title == null || body == null || clazz == null || iconId == 0) {
-      PLog.e(TAG, "Must specify non-null context, title, body, and activity class to show " +
-          "notification.");
-    } else {
-      long when = System.currentTimeMillis();
-      ComponentName name = new ComponentName(context, clazz);
-      
-      Intent intent = new Intent();
-      intent.setComponent(name);
-      intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-      
-      if (extras != null) {
-        intent.putExtras(extras);
-      }
-      
-      PendingIntent contentIntent = PendingIntent.getActivity(context, (int)when, intent, 0);
-
-      // Construct a Notification
-      notification = new Notification(iconId, body, when);
-      notification.flags |= Notification.FLAG_AUTO_CANCEL;
-      notification.defaults |= Notification.DEFAULT_ALL;
-      notification.setLatestEventInfo(context, title, body, contentIntent);
-    }
-    
-    return notification;
-  }
-  
   public void showNotification(Context context, Notification notification) {
     if (context != null && notification != null) {
       notificationCount.incrementAndGet();
@@ -149,10 +104,5 @@ import android.util.SparseIntArray;
         }
       }
     }
-  }
-  
-  public void showNotification(Context context, String title, String body,
-      Class<? extends Activity> cls, int iconId, Bundle extras) {
-    showNotification(context, createNotification(context, title, body, cls, iconId, extras));
   }
 }
